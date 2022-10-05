5984638636b5fdfd32e9a5329eeb111c6db7d086
adanilevsky <danilevskii.aleksandr97@gmail.com>
2022-10-05 22:05:36 +0300

    Update default values in Car

--- homework_02/car.py
+++ homework_02/car.py
@@ -6,8 +6,8 @@ from homework_02.engine import Engine
 
 
 class Car(Vehicle):
-    def __init__(self, *kwargs):
-        super().__init__(*kwargs)
+    def __init__(self, weight: float = 1.5, fuel: float = 0, fuel_consumption: float = 8.2):
+        super().__init__(weight=weight, fuel=fuel, fuel_consumption=fuel_consumption)
         self.engine = None
 
     def set_engine(self, engine: Engine):
