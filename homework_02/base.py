517d6855dbe02b311b0910ae264d4e77ea6e5217
adanilevsky <danilevskii.aleksandr97@gmail.com>
2022-10-05 20:23:24 +0300

    Update base.py

--- homework_02/base.py
+++ homework_02/base.py
@@ -4,14 +4,14 @@ import exceptions
 
 class Vehicle(ABC):
 
-    def __init__(self, weight: float = 0, fuel: float = 0, fuel_consumption: float = 0, started: bool = False):
+    def __init__(self, weight: float = 1000, fuel: float = 5, fuel_consumption: float = 1, started: bool = False):
         self.weight = weight
         self.fuel = fuel
         self.fuel_consumption = fuel_consumption
         self.started = started
 
     def start(self):
-        if not self.started:
+        if self.started is False:
             if self.fuel > 0:
                 self.started = True
             else:
@@ -22,12 +22,3 @@ class Vehicle(ABC):
             self.fuel -= self.fuel_consumption * distance
         else:
             raise exceptions.NotEnoughFuel
-
-
-if __name__ == '__main__':
-    v = Vehicle(fuel=1, fuel_consumption=0.2)
-    print(v.fuel, v.fuel_consumption, v.started)
-    v.start()
-    print(v.fuel, v.fuel_consumption, v.started)
-    v.move(1)
-    print(v.fuel, v.fuel_consumption, v.started)
