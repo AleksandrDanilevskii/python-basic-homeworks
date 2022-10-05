6f194712686ab6df5be92784b573c224dc2c68ce
adanilevsky <danilevskii.aleksandr97@gmail.com>
2022-10-05 22:19:09 +0300

    Update default values in Car and Plane

--- homework_02/car.py
+++ homework_02/car.py
@@ -6,7 +6,7 @@ from homework_02.engine import Engine
 
 
 class Car(Vehicle):
-    def __init__(self, weight: float = 1.5, fuel: float = 0, fuel_consumption: float = 8.2):
+    def __init__(self, weight: float = 1.5, fuel: float = 0, fuel_consumption: float = 0.082):
         super().__init__(weight=weight, fuel=fuel, fuel_consumption=fuel_consumption)
         self.engine = None
 
--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,17 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+from homework_02.base import Vehicle
+
+
+class Plane(Vehicle):
+    def __init__(self, weight: float = 200, fuel: float = 0, fuel_consumption: float = 10, max_cargo: float = 500, cargo: float = 0):
+        super().__init__(weight=weight, fuel=fuel, fuel_consumption=fuel_consumption)
+        self.cargo = cargo
+        self.max_cargo = max_cargo
+
+
+if __name__ == '__main__':
+    p = Plane()
+    print(p)
+    print(p.__dict__)
