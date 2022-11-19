5a7c1507812ca19b9ca21e9118b46b293600e1fc
adanilevsky <danilevskii.aleksandr97@gmail.com>
2022-11-19 13:20:05 +0300

    Add typing List to jsonplaceholder_requests.py

--- homework_04/jsonplaceholder_requests.py
+++ homework_04/jsonplaceholder_requests.py
@@ -1,6 +1,7 @@
 """
 создайте асинхронные функции для выполнения запросов к ресурсам (используйте aiohttp)
 """
+from typing import List
 
 import aiohttp
 
@@ -16,14 +17,14 @@ async def fetch_json(url: str):
         return data
 
 
-async def get_users(url: str = USERS_DATA_URL, fields: list[str] | None = USERS_DATA_FIELDS):
+async def get_users(url: str = USERS_DATA_URL, fields: List[str] | None = USERS_DATA_FIELDS):
     data = await fetch_json(url)
     if fields:
         return [{field: user[field] for field in fields} for user in data]
     return data
 
 
-async def get_user(user_id: int, url: str = USERS_DATA_URL, fields: list[str] | None = USERS_DATA_FIELDS):
+async def get_user(user_id: int, url: str = USERS_DATA_URL, fields: List[str] | None = USERS_DATA_FIELDS):
     data = await fetch_json(f"{url}/{user_id}")
     if fields:
         return {field: data[field] for field in fields}
