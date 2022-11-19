59ab7596cd8e345dbe6defca1f10b7a87b37151d
adanilevsky <danilevskii.aleksandr97@gmail.com>
2022-11-19 13:08:58 +0300

    Add main function

--- homework_04/main.py
+++ homework_04/main.py
@@ -13,13 +13,72 @@
 - закрытие соединения с БД
 """
 
+import asyncio
+from typing import List
+
+from sqlalchemy.ext.asyncio import AsyncEngine
+
+from homework_04.models import async_engine, Base, User, async_session, AsyncSession, Post
+from jsonplaceholder_requests import (
+    get_users,
+    get_posts,
+)
+
+
+async def create_tables(engine: AsyncEngine, metadata):
+    async with engine.begin() as conn:
+        await conn.run_sync(metadata.drop_all)
+        await conn.run_sync(metadata.create_all)
+
+    print('tables created')
+
+
+async def create_users(session: AsyncSession, users_json: List[dict]) -> list[User]:
+
+    users = [
+        User(id=user_json['id'], name=user_json['name'], username=user_json['username'], email=user_json['email'])
+        for user_json in users_json
+    ]
+    session.add_all(users)
+    await session.commit()
+    print("created users", users)
+
+    return users
+
+
+async def create_posts(session: AsyncSession, posts_json: List[dict]) -> list[Post]:
+
+    posts = [
+        Post(id=post_json['id'], title=post_json['title'], body=post_json['body'], user_id=post_json['userId'])
+        for post_json in posts_json
+    ]
+    session.add_all(posts)
+    await session.commit()
+    print("created posts", posts)
+
+    return posts
+
 
 async def async_main():
-    pass
+    # удаляем/создаем чистенькие таблицы
+    await create_tables(engine=async_engine, metadata=Base.metadata)
+
+    # получаем данные
+    users_data: List[dict]
+    posts_data: List[dict]
+    users_data, posts_data = await asyncio.gather(
+        get_users(),
+        get_posts(),
+    )
+
+    # записываем данные в DB
+    async with async_session() as session:
+        await create_users(session=session, users_json=users_data)
+        await create_posts(session=session, posts_json=posts_data)
 
 
 def main():
-    pass
+    asyncio.run(async_main())
 
 
 if __name__ == "__main__":
