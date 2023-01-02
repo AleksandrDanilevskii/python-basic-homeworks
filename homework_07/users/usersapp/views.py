8c8af47a63ab54f4518a44bf7b3eb3612d644ad6
adanilevsky <danilevskii.aleksandr97@gmail.com>
2023-01-02 16:10:59 +0300

    Add users list to index page

--- homework_07/users/usersapp/views.py
+++ homework_07/users/usersapp/views.py
@@ -2,9 +2,13 @@ import datetime
 
 from django.shortcuts import render
 
+from usersapp.models import User
+
 
 def main_page(request):
+    users = User.objects.all()
     context = {
-        'now': datetime.datetime.now()
+        'now': datetime.datetime.now(),
+        'users': users,
     }
     return render(request, 'usersapp/index.html', context=context)
