1d9e897b717d890605c725ee3e8b9cc8d968a415
adanilevsky <danilevskii.aleksandr97@gmail.com>
2023-01-02 15:08:06 +0300

    Add index page in views

--- homework_07/users/usersapp/views.py
+++ homework_07/users/usersapp/views.py
@@ -1,3 +1,10 @@
+import datetime
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def main_page(request):
+    context = {
+        'now': datetime.datetime.now()
+    }
+    return render(request, 'usersapp/index.html', context=context)
