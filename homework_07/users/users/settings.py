8820b82110fe3c97e645440c0d1da75a1ab776ca
adanilevsky <danilevskii.aleksandr97@gmail.com>
2023-01-02 20:02:12 +0300

    Add django debug toolbar

--- homework_07/users/users/settings.py
+++ homework_07/users/users/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # others
+    "debug_toolbar",
+    # my
     "usersapp",
 ]
 
@@ -48,6 +51,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "users.urls"
@@ -122,3 +126,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# django debug toolbar
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
