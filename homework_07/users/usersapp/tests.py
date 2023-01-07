0947df13eeed51328ab491be81436f33340a4182
adanilevsky <danilevskii.aleksandr97@gmail.com>
2023-01-07 22:16:08 +0300

    Add tests for models

--- homework_07/users/usersapp/tests.py
+++ homework_07/users/usersapp/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
 
-# Create your tests here.
+from usersapp.models import User, Post
+
+
+class TestUser(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create(
+            name='Alexander Danilevsky',
+            username='a_danilevsky',
+            company='BIOCAD'
+        )
+
+    def tearDown(self) -> None:
+        del self.user
+        User.objects.all().delete()
+
+    def test_str(self):
+        self.assertEqual(str(self.user), 'Alexander Danilevsky (a_danilevsky)')
+
+    # def test_phone(self):
+    #     print('phone test')
+    #     # pass
+    #     with self.assertRaises(Exception):
+    #         User.objects.create(
+    #             name='Alexander II',
+    #             username='a_second',
+    #             phone='11111 11111 11111 11111'
+    #         )
+
+
+class TestPost(TestCase):
+
+    def setUp(self) -> None:
+        self.user = User.objects.create(
+            name='Alexander Danilevsky',
+            username='a_danilevsky',
+            company='BIOCAD'
+        )
+        self.post = Post.objects.create(
+            title='Test post',
+            body='test body',
+            author=self.user
+        )
+
+    def tearDown(self) -> None:
+        Post.objects.all().delete()
+        User.objects.all().delete()
+
+    def test_str(self):
+        self.assertEqual(str(self.post), 'Test post (a_danilevsky)')
